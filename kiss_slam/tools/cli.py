8a1296f476d74ff3083ea7659baa729ec7145a1d
Meher Malladi <rm.meher97@gmail.com>
2026-02-24 14:06:23 +0100

    modify the cli for lio

--- kiss_slam/tools/cli.py
+++ kiss_slam/tools/cli.py
@@ -24,37 +24,40 @@ from pathlib import Path
 from typing import Optional
 
 import typer
-from kiss_icp.datasets import available_dataloaders, sequence_dataloaders
-from kiss_icp.tools.cmd import guess_dataloader
+import yaml
+from rko_lio.config import PipelineConfig as RKO_LIO_Config
+from rko_lio.dataloaders import available_dataloaders, dataloader_factory
+from rko_lio.util import (
+    error_and_exit,
+    info,
+    warning,
+)
 
 
 def name_callback(value: str):
+
     if not value:
         return value
     dl = available_dataloaders()
-    if value not in dl:
-        raise typer.BadParameter(f"Supported dataloaders are:\n{', '.join(dl)}")
+    if value.lower() not in [d.lower() for d in dl]:
+        raise typer.BadParameter(f"Supported dataloaders are: {', '.join(dl)}")
+    for d in dl:
+        if value.lower() == d.lower():
+            return d
     return value
 
 
 app = typer.Typer(add_completion=False, rich_markup_mode="rich")
 
-# Remove from the help those dataloaders we explicitly say how to use
-_available_dl_help = available_dataloaders()
-_available_dl_help.remove("generic")
-_available_dl_help.remove("mcap")
-_available_dl_help.remove("ouster")
-_available_dl_help.remove("rosbag")
-
 
 @app.command()
 def kiss_slam(
-    data: Path = typer.Argument(
+    data_path: Path = typer.Argument(
         ...,
         help="The data directory used by the specified dataloader",
         show_default=False,
     ),
-    dataloader: str = typer.Option(
+    dataloader_name: str = typer.Option(
         None,
         show_default=False,
         case_sensitive=False,
@@ -84,73 +87,101 @@ def kiss_slam(
         help="[Optional] For some dataloaders, you need to specify a given sequence",
         rich_help_panel="Additional Options",
     ),
-    topic: Optional[str] = typer.Option(
+    imu_topic: str | None = typer.Option(
         None,
-        "--topic",
-        "-t",
-        show_default=False,
-        help="[Optional] Only valid when processing rosbag files",
-        rich_help_panel="Additional Options",
+        "--imu",
+        help="Extra dataloader argument: imu topic",
+        rich_help_panel="Rosbag dataloader options",
     ),
-    n_scans: int = typer.Option(
-        -1,
-        "--n-scans",
-        "-n",
-        show_default=False,
-        help="[Optional] Specify the number of scans to process, default is the entire dataset",
-        rich_help_panel="Additional Options",
+    lidar_topic: str | None = typer.Option(
+        None,
+        "--lidar",
+        help="Extra dataloader argument: lidar topic",
+        rich_help_panel="Rosbag dataloader options",
     ),
-    jump: int = typer.Option(
-        0,
-        "--jump",
-        "-j",
-        show_default=False,
-        help="[Optional] Specify if you want to start to process scans from a given starting point",
-        rich_help_panel="Additional Options",
+    base_frame: str | None = typer.Option(
+        None,
+        "--base_frame",
+        help="Extra dataloader argument: base_frame for odometry estimation, default is lidar frame",
+        rich_help_panel="Rosbag dataloader options",
     ),
-    config: Optional[Path] = typer.Option(
+    imu_frame: str | None = typer.Option(
+        None,
+        "--imu_frame",
+        help="Extra dataloader argument: imu frame overload",
+        rich_help_panel="Rosbag dataloader options",
+    ),
+    lidar_frame: str | None = typer.Option(
+        None,
+        "--lidar_frame",
+        help="Extra dataloader argument: lidar frame overload",
+        rich_help_panel="Rosbag dataloader options",
+    ),
+    config_fp: Optional[Path] = typer.Option(
         None,
         "--config",
         exists=True,
         show_default=False,
         help="[Optional] Path to the configuration file",
     ),
-    meta: Optional[Path] = typer.Option(
+    log_results: bool = typer.Option(
+        False,
+        "--log",
+        "-l",
+        help="Log trajectory results to disk at 'log_dir' on completion",
+        rich_help_panel="Disk logging options",
+    ),
+    log_dir: Path | None = typer.Option(
         None,
-        "--meta",
-        "-m",
-        exists=True,
-        show_default=False,
-        help="[Optional] For Ouster pcap dataloader, specify metadata json file path explicitly",
-        rich_help_panel="Additional Options",
+        "--log_dir",
+        "-o",
+        help="Where to dump LIO results if logging. If unspecified, and logging is enabled, a folder `results` will be created in the current directory.",
+        file_okay=False,
+        dir_okay=True,
+        writable=True,
+        rich_help_panel="Disk logging options",
+    ),
+    run_name: str | None = typer.Option(
+        None,
+        "--run_name",
+        "-n",
+        help="Name prefix for output files if logging. Leave empty to take the name from the data_path argument",
+        rich_help_panel="Disk logging options",
     ),
 ):
-    # Attempt to guess some common file extensions to avoid using the --dataloader flag
-    if not dataloader:
-        dataloader, data = guess_dataloader(data, default_dataloader="generic")
+    user_rko_lio_config = {}
+    if config_fp:
+        with open(config_fp, "r") as f:
 
-    # Validate some options
-    if dataloader in sequence_dataloaders() and sequence is None:
-        print('You must specify a sequence "--sequence"')
-        raise typer.Exit(code=1)
+            user_rko_lio_config.update(yaml.safe_load(f))
+    user_rko_lio_config["log_dir"] = log_dir or user_rko_lio_config.get(
+        "log_dir", "results"
+    )
+    user_rko_lio_config["run_name"] = run_name or user_rko_lio_config.get(
+        "run_name", data_path.name
+    )
 
-    # Lazy-loading for faster CLI
-    from kiss_icp.datasets import dataset_factory
+    rko_lio_config = RKO_LIO_Config(**user_rko_lio_config)
+
+    dataloader = dataloader_factory(
+        name=dataloader_name,
+        data_path=data_path,
+        sequence=sequence,
+        imu_topic=imu_topic,
+        lidar_topic=lidar_topic,
+        imu_frame_id=imu_frame,
+        lidar_frame_id=lidar_frame,
+        base_frame_id=base_frame,
+        timestamp_processing_config=rko_lio_config.timestamps,
+    )
+    print("Loaded dataloader:", dataloader)
 
     from kiss_slam.pipeline import SlamPipeline
 
     SlamPipeline(
-        dataset=dataset_factory(
-            dataloader=dataloader,
-            data_dir=data,
-            sequence=sequence,
-            topic=topic,
-            meta=meta,
-        ),
-        config_file=config,
+        dataset=dataloader,
+        config=rko_lio_config.to_dict(),
         visualize=visualize,
-        n_scans=n_scans,
-        jump=jump,
         refuse_scans=refuse_scans,
     ).run().print()
 
